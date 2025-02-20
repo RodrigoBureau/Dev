aa8140a63e142f357687ebb15c470c0144f63adf
Rodrigo Bureau <rodrigo.bureau.t0126459@edu.atec.pt>
2025-02-20 10:29:20 +0000

    New-Feature-Exe1

--- Projetos_Rodrigo_Bureau/UFCD_10789/exercicio1.c
+++ Projetos_Rodrigo_Bureau/UFCD_10789/exercicio1.c
@@ -2,5 +2,11 @@
 
 int main(){
     printf("Hello World");
+    int Num=0;
+    for(int i=0;i<10;i++){
+        Num+2;
+        printf("Num = %d, i= %d",Num, i);
+        printf("aquilFOR");
+    }
     return 0;
 }
